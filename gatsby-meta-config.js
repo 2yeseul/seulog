7faa6b8405b1220522f1edaec6d8762ccd83a14b
2yeseul <20152917@sungshin.ac.kr>
2021-01-01 01:47:50 +0900

    init

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Seulog`,
+  description: `무한삽질`,
+  author: `2yeseul`,
+  introduction: `무한삽질기록`,
+  siteUrl: `https://seulog.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `2yeseul`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: '2yeseul/seulog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
